36e1daf190c927ecfef21dfc0cc7be71613288c4
facundomoreno <facundoivanmoreno1@gmail.com>
2023-12-29 15:44:06 -0300

    added tests and some refactors to contract

--- contracts/PollContract.sol
+++ contracts/PollContract.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.2 <0.9.0;
 
+error PollContract_NotEnoughOptionsProvided();
+error PollContract_NeededBiggerClosesAtDate(uint256 receivedTime, uint256 blockTimestamp);
 error PollContract_AlreadyVotedInPoll();
 
 contract PollContract {
@@ -30,8 +32,8 @@ contract PollContract {
         uint256 createdAt;
         uint256 closesAt;
         address creator;
+        uint256 numberOfOptions;
         PollStatus status;
-        string[] options;
         Vote[] votes;
     }
 
@@ -39,7 +41,8 @@ contract PollContract {
     mapping(uint256 => Poll) private polls;
     //address is the voter and uint256 for the pollId
     mapping(address => mapping(uint256 => Vote)) private globalVotesByAddress;
-    mapping(uint256 => Option[]) pollOptions;
+    // poll id and then option id
+    mapping(uint256 => mapping(uint256 => Option)) pollOptions;
 
     constructor() {}
 
@@ -47,60 +50,65 @@ contract PollContract {
         string memory _name,
         string memory _description,
         bool _allowMultipleOptionsSelected,
-        uint256 _createdAt,
         uint256 _closesAt,
         string[] memory _options
     ) external payable {
-        uint256 newPollId = pollsCount++;
+        uint256 aproxCurrentTime = block.timestamp;
+        if (_closesAt < aproxCurrentTime) {
+            revert PollContract_NeededBiggerClosesAtDate(_closesAt, aproxCurrentTime);
+        }
+        if (_options.length < 2) {
+            revert PollContract_NotEnoughOptionsProvided();
+        }
+
+        uint256 newPollId = pollsCount;
         Poll storage newPoll = polls[newPollId];
 
         newPoll.id = newPollId;
         newPoll.name = _name;
         newPoll.description = _description;
         newPoll.allowMultipleOptionsSelected = _allowMultipleOptionsSelected;
-        newPoll.createdAt = _createdAt;
+        newPoll.createdAt = aproxCurrentTime;
         newPoll.closesAt = _closesAt;
         newPoll.creator = msg.sender;
         newPoll.status = PollStatus.OPEN;
-        newPoll.options = _options;
+
+        newPoll.numberOfOptions = 0;
+
+        for (uint256 i = 0; i < _options.length; i++) {
+            pollOptions[newPollId][i] = Option(i, newPollId, 0, _options[i]);
+            newPoll.numberOfOptions++;
+        }
+
+        pollsCount++;
     }
 
     function votePoll(uint256 _pollId, uint256[] memory optionsVotedIds) external payable {
         // checking that sender did not already vote in poll
         address testAddress = globalVotesByAddress[msg.sender][_pollId].voter;
-        if (testAddress == address(0)) {
+        if (testAddress != address(0)) {
             revert PollContract_AlreadyVotedInPoll();
-        }
-
-        Option[] memory mOptions = new Option[](optionsVotedIds.length);
-
-        for (uint256 i = 0; i < optionsVotedIds.length; i++) {
-            if (existsInOptions(_pollId, optionsVotedIds[i])) {
-                // mOptions[mOptions.length] = pollOptions[_pollId][optionsVotedIds[i]];
-            }
         }
 
         Vote storage newVote = globalVotesByAddress[msg.sender][_pollId];
 
+        for (uint256 i = 0; i < optionsVotedIds.length; i++) {
+            Option storage optionVoted = pollOptions[_pollId][optionsVotedIds[i]];
+            newVote.optionsVoted.push(optionVoted);
+            optionVoted.numberOfVotes++;
+        }
+
         newVote.pollId = _pollId;
         newVote.voter = msg.sender;
-        newVote.optionsVoted = mOptions;
-    }
 
-    function existsInOptions(uint256 pollId, uint256 optionId) internal view returns (bool) {
-        for (uint256 i = 0; i < pollOptions[pollId].length; i++) {
-            if (optionId == pollOptions[pollId][i].optionId) {
-                return true;
-            }
-        }
-        return false;
+        polls[_pollId].votes.push(newVote);
     }
 
     function getPolls() public view returns (Poll[] memory) {
         Poll[] memory mPolls = new Poll[](pollsCount);
         for (uint256 i = 0; i < pollsCount; i++) {
-            Poll storage lBid = polls[i];
-            mPolls[i] = lBid;
+            Poll storage sPoll = polls[i];
+            mPolls[i] = sPoll;
         }
         return mPolls;
     }
@@ -111,5 +119,22 @@ contract PollContract {
 
     function getPollsCount() public view returns (uint256) {
         return pollsCount;
+    }
+
+    function getPollOptions(uint256 _pollId) public view returns (Option[] memory) {
+        Poll memory pollToRevise = getPollById(_pollId);
+
+        Option[] memory mOptions = new Option[](pollToRevise.numberOfOptions);
+
+        for (uint256 i = 0; i < pollToRevise.numberOfOptions; i++) {
+            Option storage sOption = pollOptions[_pollId][i];
+            mOptions[i] = sOption;
+        }
+
+        return mOptions;
+    }
+
+    function getMyVoteInPoll(uint256 _pollId) public view returns (Vote memory) {
+        return globalVotesByAddress[msg.sender][_pollId];
     }
 }
--- test/unit/Poll.test.ts
+++ test/unit/Poll.test.ts
@@ -6,7 +6,7 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
     ? describe.skip
     : describe("PollContract unit tests", () => {
           let pollContract: any
-          let deployer
+          let deployer: any
 
           beforeEach(async () => {
               const [deployerResult] = await ethers.getSigners()
@@ -26,15 +26,15 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
           })
 
           describe("Create new poll", () => {
-              it("creates polls and increase newPollId correctly", async () => {
+              it("creates polls, increase newPollId correctly and add options to global mapping", async () => {
                   let pollsCount
                   const now = new Date()
+
                   const threeDaysFromNow = new Date(now.setDate(now.getDate() + 3)).getTime()
                   const newPoll1 = [
                       "Quien ganara gran hermano?",
                       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                       false,
-                      now.getTime(),
                       threeDaysFromNow,
                       ["Alan", "Martin", "Joan"]
                   ]
@@ -43,7 +43,6 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                       "Va a llover mañana en la ciudad?",
                       "LLLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                       false,
-                      now.getTime(),
                       threeDaysFromNow,
                       ["Si", "No"]
                   ]
@@ -57,6 +56,90 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                   const polls = await pollContract.getPolls()
 
                   assert.equal(pollsCount, 2, polls.length)
+
+                  const pollOptionsOfFirst = await pollContract.getPollOptions(0)
+
+                  assert.equal(pollOptionsOfFirst.length, 3)
+
+                  const pollOptionsOfSecond = await pollContract.getPollOptions(1)
+
+                  assert.equal(pollOptionsOfSecond.length, 2)
+              })
+
+              it("fails if you dont send enough options to poll", async () => {
+                  let pollsCount
+                  const now = new Date()
+
+                  const threeDaysFromNow = new Date(now.setDate(now.getDate() + 3)).getTime()
+                  const newPoll1 = [
+                      "Quien ganara gran hermano?",
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                      false,
+                      threeDaysFromNow,
+                      ["Alan"]
+                  ]
+
+                  await expect(pollContract.createPoll(...newPoll1)).to.be.rejectedWith(
+                      "PollContract_NotEnoughOptionsProvided"
+                  )
+
+                  pollsCount = await pollContract.getPollsCount()
+
+                  assert.equal(pollsCount, 0)
+              })
+              it("fails if you dont send a closesAt after the aprox current time", async () => {
+                  await network.provider.send("evm_increaseTime", [100000000])
+                  await network.provider.send("evm_mine", [])
+
+                  const breakingClosesAt = 1703866244 // Tue Jan 20 1970 14:17:46 GMT-0300
+                  const newPoll1 = [
+                      "Quien ganara gran hermano?",
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                      false,
+                      breakingClosesAt,
+                      ["Alan", "Juan"]
+                  ]
+
+                  await expect(pollContract.createPoll(...newPoll1)).to.be.rejectedWith(
+                      "PollContract_NeededBiggerClosesAtDate"
+                  )
+              })
+          })
+          describe("Vote", async () => {
+              beforeEach(async () => {
+                  const now = new Date()
+
+                  const threeDaysFromNow = new Date(now.setDate(now.getDate() + 3)).getTime()
+                  const newPoll1 = [
+                      "Quien ganara gran hermano?",
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                      false,
+                      threeDaysFromNow,
+                      ["Alan", "Martin", "Joan"]
+                  ]
+                  await pollContract.createPoll(...newPoll1)
+              })
+              it("add new vote to poll, updating the option number of votes", async () => {
+                  await pollContract.votePoll(0, [0, 2])
+
+                  const voteResult = await pollContract.getMyVoteInPoll(0)
+
+                  const pollResult = await pollContract.getPollById(0)
+
+                  assert.equal(voteResult.voter, deployer.address)
+                  assert.equal(pollResult.votes.length, 1)
+
+                  const firstPollOptions = await pollContract.getPollOptions(0)
+
+                  assert.equal(firstPollOptions[0].numberOfVotes, firstPollOptions[2].numberOfVotes, 1)
+                  assert.equal(firstPollOptions[1].numberOfVotes, 0)
+              })
+              it("fails if you already vote in the poll", async () => {
+                  // first valid vote
+                  await pollContract.votePoll(0, [0, 2])
+
+                  // second vote that should fail
+                  await expect(pollContract.votePoll(0, [1])).to.be.rejectedWith("PollContract_AlreadyVotedInPoll")
               })
           })
       })
